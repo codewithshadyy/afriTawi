34a0fbc22d209c838adb14833b97012b756f032e
codewithshadyy <kipkoechshadrack10@gmail.com>
2026-05-11 10:25:51 +0300

    Setting postgressql db connection

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require("express")
+const pool  = require("./config/db")
+const app = express()
+
+const dotenv = require("dotenv")
+
+dotenv.config()
+
+
+app.use(express.json())
+
+pool.connect((err, client, release)=>{
+    if(err){
+        console.error("couldn't connect to the database", err.stack)
+       
+    } 
+
+    console.log("Database connected successfully")
+    release()
+
+    
+    
+})
+
+
+
+app.listen(process.env.PORT, () => {
+    console.log(`server running on:http://localhost:${process.env.PORT}`)
+})
