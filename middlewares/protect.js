1e4bbd3427b0402a0bf5e2b62c63983fd6b21960
codewithshadyy <kipkoechshadrack10@gmail.com>
2026-05-13 00:03:49 +0300

    Created protect middleware...

--- middlewares/protect.js
+++ middlewares/protect.js
@@ -1,13 +1,39 @@
 const jwt = require("jsonwebtoken")
+const User =  require("../models/User")
 
 
 exports.protect = async (req,res,next) => {
     try {
 
         let token;
-        if(req.headers.Authorization && req.headers.Authorization.startsWith["Bearer"]){
-            token = 
+        if(req.headers.authorization && req.headers.authorization.startsWith["Bearer"]){
+            token = req.headers.authorization.split(" ")[1]
         }
+
+        if(!token){
+            return res.status(401).json({
+                success: false,
+                message: "Not authorized. No token provided"
+            })
+        }
+
+        const decoded = jwt.verify(
+            token,
+            process.env.JWT_SECRET
+        )
+
+        const user = await User.findByPk(decoded.id)
+
+        if(!user){
+            return res.status(404).json({
+                success: false,
+                message: "User not found"
+            })
+        }
+
+        req.user = user
+
+        next()
 
         
         
