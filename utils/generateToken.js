03a7b2d80e6691feeb82eb2710f8dd96fe229c1f
codewithshadyy <kipkoechshadrack10@gmail.com>
2026-05-12 23:32:56 +0300

    Created token generator method........

---
+++ utils/generateToken.js
@@ -0,0 +1,22 @@
+const User = require("../models/User")
+const jwt = require("jsonwebtoken")
+
+
+exports.accessToken = async (user) => {
+
+    return jwt.sign(
+        {
+            id:user.id, 
+            role:user.role
+        }, 
+
+        process.env.JWT_SECRET,
+
+         {expiresIn:"15m"}
+        
+        
+        )
+    
+    
+    
+}
