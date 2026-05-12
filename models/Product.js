7144788096e86bbdeb23b04bbfeb4fe804bd1147
codewithshadyy <kipkoechshadrack10@gmail.com>
2026-05-12 22:20:37 +0300

    Product table

---
+++ models/Product.js
@@ -0,0 +1,36 @@
+const { DataTypes } = require("sequelize")
+const sequelize = require("../config/db")
+
+const Product = sequelize.define("Product", {
+
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+
+    description: {
+        type: DataTypes.TEXT
+    },
+
+    price: {
+        type: DataTypes.DECIMAL(10, 2),
+        allowNull: false
+    },
+
+    stock: {
+        type: DataTypes.INTEGER,
+        defaultValue: 1
+    },
+
+    image_url: {
+        type: DataTypes.TEXT
+    },
+
+    is_available: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: true
+    }
+
+})
+
+module.exports = Product
