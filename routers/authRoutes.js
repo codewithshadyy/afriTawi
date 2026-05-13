55508abe48ab0f19c8b8912aadefd3693f6711d6
codewithshadyy <kipkoechshadrack10@gmail.com>
2026-05-13 23:17:29 +0300

    Routed logout endpoint....

--- routers/authRoutes.js
+++ routers/authRoutes.js
@@ -1,12 +1,14 @@
 
 const express= require("express")
 const router = express.Router()
-const {signUp,signIn, verifyEmail} = require("../controllers/authController")
+const {signUp,signIn, verifyEmail, logout} = require("../controllers/authController")
 const {validateSignUp,} = require("../middlewares/validators/authValidator")
+const {protect} = require("../middlewares/protect")
 
 router.post("/signUp",validateSignUp, signUp)
 router.post("/signIn", signIn)
 router.get("/verify/:token", verifyEmail)
+router.post("/logout",protect, logout)
 
 
 
