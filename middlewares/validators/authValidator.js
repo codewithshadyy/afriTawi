2f987490ee09a32013092934de87decfd86f4977
codewithshadyy <kipkoechshadrack10@gmail.com>
2026-05-13 19:02:33 +0300

    Created authValidation middleware....

---
+++ middlewares/validators/authValidator.js
@@ -0,0 +1,36 @@
+const {body, validationResult} = require("express-validator")
+
+exports.validateSignUp = [
+    body("email")
+    .isEmail()
+    .withMessage("A valid email is required"),
+
+
+
+    body("username")
+    .notEmpty()
+    .withMessage("Email required")
+    .isLength({min:3})
+    .withMessage("Username must be atleast 3 letters"),
+
+
+    body("password")
+    .isLength({ min: 6 })
+    .withMessage("Password must be at least 6 characters"),
+    
+
+    (req,res,next) =>{
+        const errors = validationResult(req)
+
+        if(!errors.isEmpty()){
+            res.status(200).json({
+                success: false,
+                errors: errors.array()
+            })
+        }
+
+        next()
+
+    }
+]
+
