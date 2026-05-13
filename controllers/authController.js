66985b58e9fa876cbca2b36556951eb0f7559556
codewithshadyy <kipkoechshadrack10@gmail.com>
2026-05-13 23:27:01 +0300

    Fixed logout typeErrors.........

--- controllers/authController.js
+++ controllers/authController.js
@@ -239,7 +239,7 @@ exports.logout = async (req,res) => {
 
     try {
 
-        const user = await User.findByPk(req.params.id)
+        const user = await User.findByPk(req.user.id)
 
           if (!user) {
 
