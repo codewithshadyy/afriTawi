22d0bfad60d89c0e78a2017f637a697fd226357b
codewithshadyy <kipkoechshadrack10@gmail.com>
2026-05-22 14:51:11 +0300

    Paginated getCounty endpoint>>>

--- controllers/countyController.js
+++ controllers/countyController.js
@@ -82,13 +82,52 @@ exports.deleteCounty = async (req,res) => {
 
          await county.destroy()
 
-         res.json(200).json({
+         res.json(204).json({
             success:true,
             message:`county ${county.name} deleted`
          })
 
          
        
+
+        
+    } catch (error) {
+
+        res.status(500).json({
+            success:false,
+            message:error.message
+        })
+        
+    }
+    
+}
+
+
+exports.seeCounty = async (req,res) => {
+
+    try {
+          const page = parseInt(req.query.page) || 1
+          const limit = parseInt(req.query.limit) || 10
+
+          const offset = (page - 1) * limit
+
+          const {count, rows} = await County.findAndCountAll({
+            limit,
+            offset,
+            order: [["createdAt", "DESC"]]
+          })
+
+
+
+
+
+          res.status(200).json({
+            success: true,
+            totalItems: count,
+            totalPages: Math.ceil(count / limit),
+            currentPage: page,
+            data: rows
+        })
 
         
     } catch (error) {
