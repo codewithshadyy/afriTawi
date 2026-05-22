6f7e2fd659afa1d94951daaabb58709f247b988d
codewithshadyy <kipkoechshadrack10@gmail.com>
2026-05-22 14:41:41 +0300

    deleteCounty route

--- routes/countyRoutes.js
+++ routes/countyRoutes.js
@@ -1,7 +1,7 @@
 
 const express = require("express")
 const router= express.Router()
-const {createCounty, editCounty} =  require("../controllers/countyController")
+const {createCounty, editCounty, deleteCounty} =  require("../controllers/countyController")
 const { protect } = require("../middlewares/protect")
 const {adminOnly, sellerOnly} = require("../middlewares/authorizeRole")
 const {validateCounty} = require("../middlewares/validators/countyValidator")
@@ -9,6 +9,7 @@ const {validateCounty} = require("../middlewares/validators/countyValidator")
 
 router.post("/add", protect, adminOnly,validateCounty, createCounty)
 router.put("/edit/:id", protect, adminOnly,validateCounty, editCounty)
+router.delete("/delete/:id", protect,adminOnly, deleteCounty)
 
 
 
