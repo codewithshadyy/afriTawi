e5cbd9d5554df28c9fb7de3b050ef09a48716702
codewithshadyy <kipkoechshadrack10@gmail.com>
2026-05-25 20:58:11 +0300

    Routing editCategory endpoint

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -1,13 +1,14 @@
 
 const express = require("express")
 const router = express.Router()
-const {createCategory, listCategory} = require("../controllers/categoryController")
+const {createCategory, listCategory, editCategory} = require("../controllers/categoryController")
 const {protect} = require("../middlewares/protect")
 const {adminOnly} = require("../middlewares/authorizeRole")
 const {validateCategory} = require("../middlewares/validators/categoryValidator")
 
 router.post("/add", protect, adminOnly, validateCategory, createCategory)
 router.get("/list", protect, listCategory)
+router.put("/edit/:id", protect, adminOnly, validateCategory, editCategory)
 
 
 
