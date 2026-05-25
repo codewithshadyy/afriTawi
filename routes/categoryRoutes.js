b9b865b14299e1a270c1ea491386c6f15e9b7af2
codewithshadyy <kipkoechshadrack10@gmail.com>
2026-05-25 21:04:38 +0300

    Routed delete Category

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -1,7 +1,7 @@
 
 const express = require("express")
 const router = express.Router()
-const {createCategory, listCategory, editCategory} = require("../controllers/categoryController")
+const {createCategory, listCategory, editCategory, deleteCategory} = require("../controllers/categoryController")
 const {protect} = require("../middlewares/protect")
 const {adminOnly} = require("../middlewares/authorizeRole")
 const {validateCategory} = require("../middlewares/validators/categoryValidator")
@@ -9,6 +9,7 @@ const {validateCategory} = require("../middlewares/validators/categoryValidator"
 router.post("/add", protect, adminOnly, validateCategory, createCategory)
 router.get("/list", protect, listCategory)
 router.put("/edit/:id", protect, adminOnly, validateCategory, editCategory)
+post.delete("/delete/:id", protect, adminOnly, deleteCategory)
 
 
 
