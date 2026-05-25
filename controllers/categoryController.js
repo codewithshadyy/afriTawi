c01fc21420ac18020555263e1479d15f9c99726b
codewithshadyy <kipkoechshadrack10@gmail.com>
2026-05-25 19:18:59 +0300

    Built createCategory endpoint

---
+++ controllers/categoryController.js
@@ -0,0 +1,36 @@
+const Category = require("../models/Category")
+
+const {where, Op} = require("sequelize")
+
+
+exports.createCategory = async (req,res) => {
+
+    try {
+        const {name} = req.body 
+        const categoryExists = await Category.findOne({
+            where:{
+            [where.Op]:[
+                {name}
+            ]
+            }
+        })
+
+        if(categoryExists){
+            res.status(400).json({message:"Category exists"})
+        }
+       const category = await Category.create({
+        name
+       })
+
+       res.status(201).json({message:`Category ${category.name} created successfully`})
+        
+    } catch (error) {
+
+        res.status(500).json({message:error.message})
+        
+    }
+    
+}
+
+
+
