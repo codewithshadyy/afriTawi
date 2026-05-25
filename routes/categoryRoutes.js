d96c9ce8e2bc21a405ba2d3e6f65f1621a24afe1
codewithshadyy <kipkoechshadrack10@gmail.com>
2026-05-25 21:13:08 +0300

    Fixing errors

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -1,15 +1,16 @@
 
 const express = require("express")
 const router = express.Router()
-const {createCategory, listCategory, editCategory, deleteCategory} = require("../controllers/categoryController")
+const {createCategory, listCategory,findCategoryById, editCategory, deleteCategory} = require("../controllers/categoryController")
 const {protect} = require("../middlewares/protect")
 const {adminOnly} = require("../middlewares/authorizeRole")
 const {validateCategory} = require("../middlewares/validators/categoryValidator")
 
 router.post("/add", protect, adminOnly, validateCategory, createCategory)
 router.get("/list", protect, listCategory)
+router.get("/list/:id", protect, findCategoryById)
 router.put("/edit/:id", protect, adminOnly, validateCategory, editCategory)
-post.delete("/delete/:id", protect, adminOnly, deleteCategory)
+router.delete("/delete/:id", protect, adminOnly, deleteCategory)
 
 
 
