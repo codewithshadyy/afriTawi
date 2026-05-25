cdd5b3f1aca76ec6cfab586f056abf171e4f4c03
codewithshadyy <kipkoechshadrack10@gmail.com>
2026-05-25 20:17:55 +0300

    ListCategory endpoint

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -30,5 +30,37 @@ exports.createCategory = async (req,res) => {
     
 }
 
+exports.listCategory = async (req,res) => {
+
+    try {
+          const page = parseInt(req.query.page) || 1
+          const limit = parseInt(req.query.limit) || 10
+          const offset= (page -1) * limit
+
+          const category = await Category.findAndCountAll({
+            limit,
+            offset,
+            order: [["createdAt", "DESC"]]
+          })
+
+
+          res.status(200).json({
+            success:true,
+            totalItems: count,
+            totalPages: Math.ceil(count / limit),
+            currentPage: page,
+            data: rows
+
+          })
+
+        
+    } catch (error) {
+
+        res.status(500).json({message:error.message})
+        
+    }
+    
+}
+
 
 
