56588a1e8df0acd4111ef7429e7e28c278efa087
codewithshadyy <kipkoechshadrack10@gmail.com>
2026-05-26 09:58:55 +0300

    Routed listProfiles endpoint

--- routes/profileRoutes.js
+++ routes/profileRoutes.js
@@ -1,11 +1,12 @@
 
 const express = require("express")
 const router = express.Router()
-const {  createProfile  } = require("../controllers/profileController")
+const {  createProfile, listProfiles } = require("../controllers/profileController")
 const {protect} = require("../middlewares/protect")
 
 
 router.post("/create",protect, createProfile)
+router.get("/list", protect, listProfiles)
 
 
 module.exports = router
