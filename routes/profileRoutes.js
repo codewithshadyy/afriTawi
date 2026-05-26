280441a0b67ae9d13322170f5edf158efc3caadc
codewithshadyy <kipkoechshadrack10@gmail.com>
2026-05-26 18:35:49 +0300

    Added upload middlewae in the routes

--- routes/profileRoutes.js
+++ routes/profileRoutes.js
@@ -3,12 +3,13 @@ const express = require("express")
 const router = express.Router()
 const {  createProfile, listProfiles, findProfileById, editProfile } = require("../controllers/profileController")
 const {protect} = require("../middlewares/protect")
+const {upload} = require("../middlewares/upload")
 
 
-router.post("/create",protect, createProfile)
+router.post("/create",protect,upload.single("avatar"), createProfile)
 router.get("/list", protect, listProfiles)
 router.get("/list/:id", protect, findProfileById)
-router.put("/edit/:id", protect, editProfile)
+router.put("/edit/:id", protect, upload.single("avatar"), editProfile)
 
 
 module.exports = router
