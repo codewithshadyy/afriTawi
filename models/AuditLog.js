d65c3d40362b1b3b1e45cbfe2831617974f5b426
codewithshadyy <kipkoechshadrack10@gmail.com>
2026-05-28 15:12:20 +0300

    Created auditLog model

---
+++ models/AuditLog.js
@@ -0,0 +1,23 @@
+const {DataTypes} = require("sequelize")
+
+const sequelize = require("../config/db")
+
+const AuditLog = sequelize.define("AuditLog", {
+    action:{
+        type:DataTypes.STRING,
+        allowNull:false
+    },
+    entity:{
+        type:DataTypes.STRING
+    },
+    entity_id:{
+        type:DataTypes.INTEGER
+    },
+
+    details:{
+        type:DataTypes.INTEGER
+    }
+})
+
+
+module.exports =AuditLog
