7792e0e85ecadac342b39a38cbc7eea1b74ff3de
codewithshadyy <kipkoechshadrack10@gmail.com>
2026-06-02 12:17:06 +0300

    documented countyRoutes and logRoutes

--- routes/logsRoute.js
+++ routes/logsRoute.js
@@ -13,6 +13,24 @@ const{getLogs} = require("../controllers/logController")
 const {protect} = require("../middlewares/protect")
 const {adminOnly, sellersOnly} = require("../middlewares/authorizeRole")
 
+
+
+
+/**
+ * @swagger
+ * /api/logs:
+ *   get:
+ *     summary: Get all audit logs
+ *     tags: [Audit Logs]
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: Logs fetched successfully
+ *       403:
+ *         description: Admin access required
+ */
+
 router.get("/", protect, adminOnly, getLogs)
 
 
